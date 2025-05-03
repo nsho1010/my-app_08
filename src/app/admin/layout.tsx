06a09d20c092659341534eb50c5efe62a05a715f
Shohei Nagamura <shohei.nagamura@gmail.com>
2025-05-04 02:48:44 +0900

    管理画面レイアウトリファクタ

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+import Link from "next/link";
+
+type Props = {
+  children: ReactNode;
+};
+
+export const AdminLayout = ({ children }: Props) => {
+  return (
+    <div className="flex min-h-screen">
+      <aside className="w-64 bg-gray-100 p-4">
+        <ul>
+          <li>
+            <Link
+              href="/admin/posts"
+              className="block mb-2 text-blue-600 font-bold"
+            >
+              記事一覧
+            </Link>
+          </li>
+          <li>
+            <Link href="/admin/categories" className="block mb-2 text-gray-600">
+              カテゴリー一覧
+            </Link>
+          </li>
+        </ul>
+      </aside>
+      <main className="flex-1 p-8">{children}</main>
+    </div>
+  );
+};
+
+export default AdminLayout;
