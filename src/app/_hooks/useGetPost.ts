14cd6dd0e2efae63cc488f64c99a20bac26e21b5
Shohei Nagamura <shohei.nagamura@gmail.com>
2025-05-04 01:23:55 +0900

    記事詳細ページをリファクタ

--- src/app/_hooks/useGetPost.ts
+++ src/app/_hooks/useGetPost.ts
@@ -1,33 +1,18 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
-import { BASE_URL } from "../_constants";
-import { Post } from "../_types";
+import useSWR from "swr";
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export const useGetPost = (id: string) => {
-  const [post, setPost] = useState<Post | null>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { data, error, isLoading } = useSWR(
+    `${API_URL}/api/posts/${id}`,
+    fetcher
+  );
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const res = await axios.get(`${BASE_URL}/posts/${id}`, {
-          headers: {
-            "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_API_KEY,
-          },
-        });
-        setPost(res.data);
-      } catch (err) {
-        setError("記事の取得に失敗しました。");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    if (id) {
-      fetchPost();
-    }
-  }, [id]);
-
-  return { post, error, isLoading };
+  return {
+    data,
+    error,
+    isLoading,
+  };
 };
--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,6 +2,7 @@
 
 import { format } from "date-fns";
 import { useGetPost } from "../../_hooks/useGetPost";
+import { PostCategory } from "@/app/_types";
 
 type Props = {
   params: {
@@ -10,45 +11,47 @@ type Props = {
 };
 
 const ArticleDetail = ({ params }: Props) => {
-  const { post, error, isLoading } = useGetPost(params.id);
+  const { data, error, isLoading } = useGetPost(params.id);
 
   if (!params.id) return <div>記事IDが見つかりませんでした。</div>;
 
   if (isLoading) return <div>読み込み中...</div>;
 
-  if (error) return <div>{error}</div>;
+  if (error) return <div>エラーが発生しました。</div>;
 
-  if (!post) return <div>記事が見つかりませんでした。</div>;
+  if (!data) return <div>記事が見つかりませんでした。</div>;
 
   return (
     <div className="w-full max-w-3xl mx-auto flex flex-col items-center pt-16 pb-8 min-h-[calc(100vh-4rem)]">
       <img
-        src={post.thumbnail.url}
+        src={data.post.thumbnailUrl}
         alt="データから取得した画像"
         className="mb-4 max-h-[40vh] object-contain"
       />
       <div className="p-4 w-full">
         <div className="flex justify-between">
           <p className="text-gray-500">
-            {format(new Date(post.createdAt), "yyyy/MM/dd")}
+            {format(new Date(data.post.createdAt), "yyyy/MM/dd")}
           </p>
           <div className="flex">
-            {post.categories.map((category, catIndex) => (
-              <div
-                key={catIndex}
-                className="text-blue-600 border-2 border-blue-600 rounded-md py-[0.2rem] px-[0.4rem] mr-2 last:mr-0"
-              >
-                {category.name}
-              </div>
-            ))}
+            {data.post.postCategories.map(
+              (postCategory: PostCategory, catIndex: number) => (
+                <div
+                  key={catIndex}
+                  className="text-blue-600 border-2 border-blue-600 rounded-md py-[0.2rem] px-[0.4rem] mr-2 last:mr-0"
+                >
+                  {postCategory.category.name}
+                </div>
+              )
+            )}
           </div>
         </div>
         <div>
-          <h2 className="text-2xl font-bold mt-2 mb-4">{post.title}</h2>
+          <h2 className="text-2xl font-bold mt-2 mb-4">{data.post.title}</h2>
           <div
             className="text-base"
             dangerouslySetInnerHTML={{
-              __html: post.content,
+              __html: data.post.content,
             }}
           ></div>
         </div>
