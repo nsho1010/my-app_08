7a9b8a1066f8efd948280d4597cf8693e447f13f
Shohei Nagamura <shohei.nagamura@gmail.com>
2025-05-04 01:05:10 +0900

    不要コードの削除

--- src/app/_hooks/useGetAllPosts.ts
+++ src/app/_hooks/useGetAllPosts.ts
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
-import { BASE_URL } from "../_constants";
-import { Post, GetPostsResponse } from "../_types";
+import { GetPostsResponse } from "../_types";
 import useSWR from "swr";
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const fetcher = (url: string) => axios.get(url).then((res) => res.data);
 
 export const useGetAllPosts = () => {
   const { data, error, isLoading } = useSWR<GetPostsResponse>(
-    "http://localhost:3030/api/posts",
+    `${API_URL}/api/posts`,
     fetcher
   );
 
